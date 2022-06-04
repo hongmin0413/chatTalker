4906ed0beef57c042206694c83a945dba97732b5
hongmin0413 <101363407+hongmin0413@users.noreply.github.com>
2022-06-05 00:20:10 +0800

    price.html新增選擇方案及相應的css、js

--- js/all.js
+++ js/all.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  //卡片輪播
   $('.feedbackList').slick({
     arrows: false,
     dots: true,
@@ -24,6 +25,27 @@ $(document).ready(function () {
   });
 })
 
+//type: [quantity, basicPrice, standardPrice]
+const plan = {"1": ["10000", "600", "1600"],
+              "2": ["15000", "500", "1500"],
+              "3": ["20000", "400", "1400"],
+              "4": ["25000", "300", "1300"],
+              "5": [">25000", "200", "1200"]};
+
+//換用戶數及相應費用
+function changePlan(type) {
+  //更換資料
+  var resultList = plan[type];
+  $(".peopleQuantity").html(resultList[0]+"<sub>位</sub>");
+  for(var i = 1; i < resultList.length; i++)
+    $(".planModeList li:nth-child("+i+") .price").html(resultList[i]+"<sub>TWD</sub>");
+
+  //更換按鈕樣式(移除現有的、點選的加active)
+  $(".planBtnList").find(".planBtn-active").removeClass("planBtn-active");
+  $(".planBtnList li:nth-child("+type+") .planBtn").addClass("planBtn-active");
+}
+
+//返回最上面
 function moveTop() {
   $("html, body").scrollTop(0, 2000);
 }
