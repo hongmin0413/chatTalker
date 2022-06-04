3de6dda721ef13f7778605eb29ece635474bec42
hongmin0413 <101363407+hongmin0413@users.noreply.github.com>
2022-06-04 16:44:31 +0800

    Add files via upload
    首頁視覺修改

--- js/all.js
+++ js/all.js
@@ -2,18 +2,22 @@ $(document).ready(function () {
   $('.feedbackList').slick({
     arrows: false,
     dots: true,
+    speed: 500,
     slidesToShow: 3,
+    slidesToScroll: 3,
     responsive: [
       {
         breakpoint: 920,
         settings: {
-          slidesToShow: 2
+          slidesToShow: 2,
+          slidesToScroll: 2
         }
       },
       {
         breakpoint: 550,
         settings: {
-          slidesToShow: 1
+          slidesToShow: 1,
+          slidesToScroll: 1
         }
       }
     ]
@@ -21,5 +25,5 @@ $(document).ready(function () {
 })
 
 function moveTop() {
-  $("html, body").scrollTop(0, 500);
+  $("html, body").scrollTop(0, 2000);
 }
