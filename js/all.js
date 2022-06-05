dd7ec79040a3e2cfe417b80e12f45470f742cc4b
hongmin0413 <101363407+hongmin0413@users.noreply.github.com>
2022-06-05 15:58:18 +0800

    plan物件化

--- js/all.js
+++ js/all.js
@@ -1,5 +1,16 @@
+class Plan {
+  constructor(quantity, basicPrice, standardPrice){
+    this.quantity = quantity;
+    this.basicPrice = basicPrice;
+    this.standardPrice = standardPrice;
+  }
+}
+
+//放不同的方案
+var planList = [];
+
 $(document).ready(function () {
-  //卡片輪播
+  //初始化卡片輪播
   $('.feedbackList').slick({
     arrows: false,
     dots: true,
@@ -23,38 +34,36 @@ $(document).ready(function () {
       }
     ]
   });
-})
 
-//type: [index, basicPrice, standardPrice]
-const plan = {"1": ["10000", "600", "1600"],
-              "2": ["15000", "500", "1500"],
-              "3": ["20000", "400", "1400"],
-              "4": ["25000", "300", "1300"],
-              "5": [">25000", "200", "1200"]};
+  //初始化方案費用並放到list
+  planList.push(new Plan("10000", 600, 1600));
+  planList.push(new Plan("15000", 500, 1500));
+  planList.push(new Plan("20000", 400, 1400));
+  planList.push(new Plan("25000", 300, 1300));
+  planList.push(new Plan(">25000", 200, 1200));
+})
 
 //換用戶數及相應費用
 function changePlan(object) {
-  var index = $(object).parent().index()+1;
-
   //更換資料
-  var resultList = plan[index];
-  $(".peopleQuantity").html(resultList[0]+"<sub>位</sub>");
-  for(var i = 1; i < resultList.length; i++)
-    $(".planModeList li:nth-child("+i+") .price").html(resultList[i]+"<sub>TWD</sub>");
+  var plan = planList[$(object).parent().index()];
+  $(".peopleQuantity").html(plan.quantity+"<sub>位</sub>");
+  $(".planModeList li:nth-child(1) .price").html(plan.basicPrice+"<sub>TWD</sub>");
+  $(".planModeList li:nth-child(2) .price").html(plan.standardPrice+"<sub>TWD</sub>");
 
   //更換按鈕樣式(移除現有的、點選的加active)
   $(".planBtnList").find(".planBtn-active").removeClass("planBtn-active");
-  $(".planBtnList li:nth-child("+index+") .planBtn").addClass("planBtn-active");
+  $(object).addClass("planBtn-active");
 }
 
 //常見問題的slide
 function slideToggle(object) {
-  //其他向上、換回加號、question換回顏色
+  //其他向上、換回加號、questionWord換回顏色
   $(object).siblings().find(".answer").slideUp(500);
   $(object).siblings().find(".subColor").siblings().toggleClass("d-none");
   $(object).siblings().find(".subColor").removeClass("subColor");
 
-  //點選的向下、換成減號、question換色
+  //點選的向下、換成減號、questionWord換色
   $(object).find(".answer").slideToggle(500);
   $(object).find("img").toggleClass("d-none");
   $(object).find(".questionWord").toggleClass("subColor");
